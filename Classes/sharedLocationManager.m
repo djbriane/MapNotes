1a9a4f9495049f0b5422f2620ef8efa2fe345406
Brian Erickson <djbriane@gmail.com>
2009-08-07 09:44:56 -0600

    Refactoring continued

---
+++ Classes/sharedLocationManager.m
@@ -0,0 +1,18 @@
+//
+//  sharedLocationManager.m
+//  MapNotes
+//
+//  Created by Brian Erickson on 8/6/09.
+//  Copyright 2009 WRKSHP, LLC. All rights reserved.
+//
+
+#import "sharedLocationManager.h"
+#import "SynthesizeSingleton.h"
+
+@implementation sharedLocationManager
+
+SYNTHESIZE_SINGLETON_FOR_CLASS(sharedLocationManager);
+
+
+
+@end
